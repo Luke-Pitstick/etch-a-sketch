f89e5f9f1103473794d9e6de59191fa910963289
Luke Pitstick <lukepitstick06@gmail.com>
2022-05-07 13:56:33 -0500

    added styling and the basic functionality of the squares

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const gridContainer = document.querySelector("#grid-container")
+const reset = document.querySelector("#id")
+const color = document.querySelector("#color")
 
 
 for (let i = 0; i < 16 ** 2; i++) {
     const square = document.createElement('div')
+
+    square.addEventListener('mouseover', () => {
+        square.classList.add('black')
+
+    })
+
     square.classList.add('white')
     square.classList.add('square')
     gridContainer.appendChild(square)
-    console.log(i);         
 }
+
+
 
